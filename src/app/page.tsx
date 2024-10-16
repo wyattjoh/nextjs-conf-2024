c678791b93cf583c12bfe9663f105e55fa1e0b88
Wyatt Johnson <accounts+github@wyattjoh.ca>
2024-10-16 11:33:28 -0600

    feat: added prisma

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,102 @@
-import Image from "next/image";
+import { PrismaClient } from "@prisma/client";
+import { revalidatePath } from "next/cache";
+import { connection } from "next/server";
+import { Suspense } from "react";
+
+const prisma = new PrismaClient();
+
+async function update() {
+  "use server";
+
+  const contributors = await fetch(
+    "https://api.github.com/repos/vercel/next.js/contributors"
+  ).then((res) => res.json());
+
+  await prisma.$transaction([
+    prisma.user.deleteMany(),
+    prisma.user.createMany({
+      data: contributors.map(
+        (contributor: {
+          id: number;
+          login: string;
+          contributions: number;
+        }) => ({
+          id: contributor.id,
+          name: contributor.login,
+          contributions: contributor.contributions,
+        })
+      ),
+    }),
+  ]);
+
+  revalidatePath("/");
+}
+
+function ContributorsSkeleton({ take }: { take: number }) {
+  return (
+    <>
+      <ul className="flex flex-col gap-2 min-w-[500px]">
+        {Array.from({ length: take }).map((_, i) => (
+          <li key={i} className="w-full h-[24px] bg-gray-50  animate-pulse" />
+        ))}
+      </ul>
+      <div className="text-xs text-gray-500">
+        <p className="flex items-center">
+          Time taken:{" "}
+          <span className="inline-block bg-gray-50 w-20 h-4 animate-pulse ml-1" />
+        </p>
+      </div>
+    </>
+  );
+}
+
+async function Contributors({ take }: { take: number }) {
+  // We always want the following database query to be uncached.
+  await connection();
+
+  const start = performance.now();
+  const contributors = await prisma.user.findMany({
+    orderBy: { contributions: "desc" },
+    take,
+  });
+  const end = performance.now();
+
+  return (
+    <>
+      <ul className="flex flex-col gap-2 min-w-[500px]">
+        {contributors.map((contributor) => (
+          <li key={contributor.id}>
+            <div className="flex justify-between items-center">
+              {contributor.name}
+              <span className="text-xs">{contributor.contributions}</span>
+            </div>
+          </li>
+        ))}
+      </ul>
+      <div className="text-xs text-gray-500">
+        <p>Time taken: {(end - start).toFixed(2)}ms</p>
+      </div>
+    </>
+  );
+}
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+      <main className="flex flex-col gap-8 row-start-2 items-end sm:items-start">
+        <h1 className="text-4xl font-bold">Next.js Contributors</h1>
+        <Suspense fallback={<ContributorsSkeleton take={20} />}>
+          <Contributors take={20} />
+        </Suspense>
+        <form action={update}>
+          <button
+            type="submit"
+            className="border border-gray-500 text-gray-500 px-2 py-1 rounded-full text-xs hover:bg-gray-50"
           >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+            Update Contributors
+          </button>
+        </form>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
